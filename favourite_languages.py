382db33633d23689f13db17537d55a51746893fd
renae-stl <74110191+renae-stl@users.noreply.github.com>
2020-11-29 15:06:13 +0000

    Add files via upload

---
+++ favourite_languages.py
@@ -0,0 +1,27 @@
+#Usisng dictionaries
+favourite_languages = {
+    'jen': 'french',
+    'pete':'spanish',
+    'greg': 'C++',
+    'sue': 'javascript',
+}
+while True:
+    for name, language in favourite_languages.items():
+        print(name.title() + "'s favourite language is " + language.title() + '\n' )
+
+    user = input('what is your name? (to quit type q) ')
+    
+    if user == 'q':
+        print('quit')
+        break
+    
+    fave_language = input('what is your favourite language? ')
+
+
+    print ('\nadding you to the dictionary...\n')
+
+    favourite_languages[user] = fave_language
+
+    for name, language in favourite_languages.items():
+        print(name.title() + "'s favourite language is " + language.title() + '\n')
+
